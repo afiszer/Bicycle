ae691c8d62f65a1bac0b4a36e9e5d7d714bac2d9
afiszer <66952919+afiszer@users.noreply.github.com>
2021-04-09 21:12:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,24 @@
-//if ('serviceWorker' in navigator) {
-  // Register a service worker hosted at the root of the
-  // site using the default scope.
- // navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    //console.log('Service worker registration succeeded:', registration);
- // }, /*catch*/ function(error) {
-    //console.log('Service worker registration failed:', error);
-  //});
-//} else {
-  //console.log('Service workers are not supported.');
-//}
-if ('serviceWorker' in navigator) {
-  // declaring scope manually
-  self.addEventListener('fetch',() => console.log("fetch"));
-  navigator.serviceWorker.register('/sw.js', {scope: '/index.html'}).then(function(registration) {
-    console.log('Service worker registration succeeded:', registration);
-  }, /*catch*/ function(error) {
-    console.log('Service worker registration failed:', error);
-  });
-} else {
-  console.log('Service workers are not supported.');
-}
+var CACHE_NAME = "bicycle-cache";
+var urlsToCache = ["."];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
